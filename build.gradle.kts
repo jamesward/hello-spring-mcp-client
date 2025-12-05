114973062461a1887c43522836937f5f4f1bb362
James Ward <james@jamesward.com>
2025-12-05 11:35:10 -0700

    bump spring ai to 1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.21"
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "3.5.8"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.10.6"
 }
@@ -14,13 +14,7 @@ kotlin {
 }
 
 dependencies {
+    implementation(platform("org.springframework.ai:spring-ai-bom:1.1.1"))
     implementation("org.springframework.ai:spring-ai-starter-model-bedrock-converse")
     implementation("org.springframework.ai:spring-ai-starter-mcp-client")
 }
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.ai:spring-ai-bom:1.1.0")
-    }
-}
-
